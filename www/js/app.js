78f5094a697c99e2e49b590ecec030efb299eb89
BillTsarnas <devtsar@gmail.com>
2016-10-05 04:35:02 +0300

    added gmap perf count

--- www/js/app.js
+++ www/js/app.js
@@ -57,6 +57,12 @@ app.controller('main_activity', function($scope, $ionicModal, $http) {
   $scope.formData={};
   
   $scope.stats="";
+  
+  $scope.noOfshops="";
+  
+  $scope.map_init_time="";
+  
+  
 
 	
 	$scope.showJson = function(){
@@ -75,14 +81,29 @@ app.controller('main_activity', function($scope, $ionicModal, $http) {
 								 };
 	$scope.showGMAP = function(){
 									$scope.GMAPModal.show();
-									navigator.geolocation.getCurrentPosition($scope.onGeoSuccess, $scope.onGeoError);
+									//navigator.geolocation.getCurrentPosition($scope.onGeoSuccess, $scope.onGeoError);
+									var center = {coords : {latitude : 38.246639, longitude : 21.734573}};
+									
+									//otan ta dedomena metatrepontai se json format, o kwdikas den paizei
+									
+									$http.get("http://83.212.86.247/thesis/getshops.php")
+									.then(function(response){ /*init map and count init time*/  $scope.onGeoSuccess(center, response.data, this); }
+									,function(response){});
+									
 								 };
 	$scope.hideGMAP = function(){
 									$scope.GMAPModal.hide();
 								 };
 	$scope.showLeaflet = function(){
 									$scope.LeafletModal.show();
-									navigator.geolocation.getCurrentPosition($scope.onGeoSuccessLeaflet, $scope.onGeoError);
+									//navigator.geolocation.getCurrentPosition($scope.onGeoSuccessLeaflet, $scope.onGeoError);
+									
+									var center = {coords : {latitude : 38.246639, longitude : 21.734573}};
+									
+									$http.get("http://83.212.86.247/thesis/getshops.php")
+									.then(function(response){ $scope.onGeoSuccessLeaflet(center, response.data); }
+									,function(response){});
+									
 								 };
 	$scope.hideLeaflet = function(){
 									$scope.LeafletModal.hide();
@@ -122,38 +143,79 @@ app.controller('main_activity', function($scope, $ionicModal, $http) {
 								 };
 								 
 	$scope.timer = function(taskType, time){
-									var obj = {task : taskType, execution : time};
+									var obj = {task : taskType, execution : time, platform : 'HYBRID'};
 									
 									$http.post("http://83.212.86.247/thesis/test.php", obj)
 									.then(function(response){}
 									,function(response){});
 								 };
 	
-	$scope.onGeoSuccess = function(position) {
+	$scope.onGeoSuccess = function(position, shops, parentScope) {
 		
-        var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+		
+		
+		
+        //center of the maps is in Patras
+		var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+		
+		
  
 		var mapOptions = {
 			center: latLng,
-			zoom: 15,
+			zoom: 13,
 			mapTypeId: google.maps.MapTypeId.ROADMAP
 		};
  
 		$scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
 		
+		//var time_init = -500;
+		//$scope.map_init_time="tsifsa motren"; //this will count the map init time
 		//Wait until the map is loaded
 		google.maps.event.addListenerOnce($scope.map, 'idle', function(){
- 
-			var marker = new google.maps.Marker({
-				map: $scope.map,
-				animation: google.maps.Animation.DROP,
-				position: latLng
-			});      
- 
+			
+			
+			
+			//pinakas markers
+			//var markers[];
+			var markers = new Array(100);
+			
+			//var start = new Date().getTime();
+			for(var i = 0; i < 100; i++){
+				
+				var lat_lng = new google.maps.LatLng(shops[i].lat, shops[i].lon);
+				
+				markers[i] = new google.maps.Marker({
+					position: lat_lng
+				});
+				
+				/*var marker = new google.maps.Marker({
+					position: lat_lng,
+					map: $scope.map
+				});*/
+				    
+			}
+			//var time_init = new Date().getTime() - start;
+			
+			var start = new Date().getTime();
+			for(var i = 0; i < 100; i++){
+				markers[i].setMap($scope.map);
+				     
+			}
+			var time_init = new Date().getTime() - start;
+			console.log("Map init time: " + time_init);
+			
+			$scope.map_init_time = time_init;
+			$scope.$apply();
+			//console.log("Map init time: " + $scope.map_init_time);
+			
+			/*return time_init;*/
 		});
+		
+		//return time_init;
+		
     };
 	
-	$scope.onGeoSuccessLeaflet = function(position) {
+	$scope.onGeoSuccessLeaflet = function(position, shops) {
 		var mymap = L.map('leaf_map').setView([position.coords.latitude, position.coords.longitude], 13);
 		
 		L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
@@ -163,7 +225,20 @@ app.controller('main_activity', function($scope, $ionicModal, $http) {
 			accessToken: 'pk.eyJ1IjoiYmlsbHRzIiwiYSI6ImNpcmMweDE3eTAwNmVpa25udjgxNWNtc3MifQ.dQaFufbf1N_440j8yKVTPA'
 		}).addTo(mymap);
 		
-		var marker = L.marker([position.coords.latitude, position.coords.longitude]).addTo(mymap);
+		//var marker = L.marker([position.coords.latitude, position.coords.longitude]).addTo(mymap);
+		
+		
+		
+		//var marker = L.marker([json[0].lat, json[0].lon]).addTo(mymap);
+ 
+			for(var i = 0; i < 100; i++){
+				
+				var marker = L.marker([shops[i].lat, shops[i].lon]).addTo(mymap);
+			
+			}
+			
+			//$scope.noOfshops = shops.length;
+			
         
     };
 
